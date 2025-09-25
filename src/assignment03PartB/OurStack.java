a0f056637cb7ea2e15c4245803d83bd6d3169019
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-09-24 17:34:44 -0700

    Implemented OurStack

--- src/assignment03PartB/OurStack.java
+++ src/assignment03PartB/OurStack.java
@@ -12,28 +12,60 @@
 
 package assignment03PartB;
 
-public class OurStack<T> implements StackInterface<T> {
+public class OurStack<T> implements StackInterface<T> 
+{
+    private Node firstNode;
 
-    public OurStack() {
+    public OurStack() {}
+
+    public OurStack(T[] data)
+    {
+        for(T entry : data)
+            this.push(entry);
     }
 
     @Override
-    public void push(T newEntry) {
+    public void push(T newEntry)
+    {
+        firstNode = new Node(newEntry, firstNode);
     }
 
     @Override
-    public T peek() {
+    public T peek()
+    {
+        return firstNode.data;
     }
 
     @Override
-    public T pop() {
+    public T pop()
+    {
+        Node topNode = firstNode;
+        firstNode = topNode.next;
+        
+        return topNode.data;
     }
 
     @Override
-    public boolean isEmpty() {
+    public boolean isEmpty()
+    {
+        return firstNode == null;
     }
 
     @Override
-    public void clear() {
+    public void clear()
+    {
+        firstNode = null;
+    }
+
+    private class Node
+    {
+        private T data;
+        private Node next;
+
+        private Node(T data, Node next)
+        {
+            this.data = data;
+            this.next = next;
+        }
     }
 }
