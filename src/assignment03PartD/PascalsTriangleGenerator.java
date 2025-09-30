74f824878aef58685e71705c978dc6b873124d31
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-09-30 15:39:00 -0700

    implemented first pass of PascalsTriangleGenerator

--- src/assignment03PartD/PascalsTriangleGenerator.java
+++ src/assignment03PartD/PascalsTriangleGenerator.java
@@ -5,17 +5,42 @@
  * File Name: PascalsTriangleGenerator.java
  * Author: Java Foundation
  * Author: Duc Ta
- * Author: <First Name> <Last Name>
+ * Author: Robin Lane
  * **********************************************
  */
 
 package assignment03PartD;
 
-public class PascalsTriangleGenerator {
+public class PascalsTriangleGenerator 
+{
+    private int[] row = {1};
 
-    public PascalsTriangleGenerator() {
+    public PascalsTriangleGenerator() {}
+
+    public int[] computeRow(int rowToCompute) 
+    {
+        if(rowToCompute > 1)
+            row = computeRow(rowToCompute - 1);
+        
+        return rowSum();
     }
 
-    public int[] computeRow(int rowToCompute) {
+    private int[] rowSum()
+    {
+        int[] newRow = new int[row.length + 1];
+
+        for(int i = 0; i < newRow.length; i++)
+            newRow[i] = safeGet(row, i) + safeGet(row, i-1);
+
+        return newRow;
+    }
+
+    // Get an item from an array, but return 0 if the index is out of bounds instead of crashing
+    private int safeGet(int [] array, int index)
+    {
+        if(index < 0 || index >= array.length)
+            return 0;
+        
+        return array[index];
     }
 }
