359773efb071b23385a4be2d47031d3a2b31d6db
Robin Lane <183433480+RubeculaBee@users.noreply.github.com>
2025-10-01 16:30:23 -0700

    expanded algorithm in Assignment03PartE01.java

--- src/assignment03PartE/Assignment03PartE01.java
+++ src/assignment03PartE/Assignment03PartE01.java
@@ -11,10 +11,30 @@
 package assignment03PartE;
 
 public class Assignment03PartE01 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        originalAlgorithm();
+        System.out.println();
+        expandedAlgorithm();
+    }
+
+    static void originalAlgorithm()
+    {
         int i, n = 5, sum = 5;
         for (i = 5; i < 3 * n; i++) {
             sum *= n + i * 7 + 37;
+        }
+        System.out.println(sum);   
+    }
+
+    static void expandedAlgorithm()
+    {
+        int i, n, sum;
+        n = 5;
+        sum = 5;
+        for(i=5; i < 3 * n; i = i + 1)
+        {
+            sum = sum * (n + i * 7 + 37);
         }
         System.out.println(sum);
     }
